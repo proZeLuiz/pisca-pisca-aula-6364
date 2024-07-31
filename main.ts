e6720f142f792848c23d5e29ce2b0402b62dc65c
proZeLuiz <profzeluiz.pires@gmail.com>
2024-07-31 14:52:43 -0300

    pisca pisca eterno

--- main.ts
+++ main.ts
@@ -1,34 +1,33 @@
-basic.showLeds(`
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    `)
-led.setBrightness(58)
-basic.showLeds(`
-    # . # . #
-    . # . # .
-    # . # . #
-    . # . # .
-    # . # . #
-    `)
-led.setBrightness(255)
-basic.showLeds(`
-    # # # # #
-    . . . . .
-    # # # # #
-    . . . . .
-    # # # # #
-    `)
-led.setBrightness(255)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
 basic.forever(function () {
-	
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    led.setBrightness(58)
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    led.setBrightness(255)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        # # # # #
+        . . . . .
+        # # # # #
+        `)
+    led.setBrightness(255)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
