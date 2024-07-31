038fba3cf09967ea63faf58acef8ab1a1c0d5e6e
proZeLuiz <profzeluiz.pires@gmail.com>
2024-07-31 14:51:43 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+led.setBrightness(58)
+basic.showLeds(`
+    # . # . #
+    . # . # .
+    # . # . #
+    . # . # .
+    # . # . #
+    `)
+led.setBrightness(255)
+basic.showLeds(`
+    # # # # #
+    . . . . .
+    # # # # #
+    . . . . .
+    # # # # #
+    `)
+led.setBrightness(255)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
